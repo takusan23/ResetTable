4db91963f861254fabbf3593c256fa9cafb6344b
takusan23 <takumu1200@gmail.com>
2026-06-27 01:43:28 +0900

    いらないファイルを消す

--- src/client/kotlin/io/github/takusan23/resettable/client/ResetTableClient.kt
+++
@@ -1,9 +0,0 @@
-package io.github.takusan23.resettable.client
-
-import net.fabricmc.api.ClientModInitializer
-
-object ResetTableClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
